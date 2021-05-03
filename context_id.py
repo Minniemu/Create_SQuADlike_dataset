6ba81be7e0321bc90f0a1e2c363b792288adfbac
Minnie Mu <68094429+Minniemu@users.noreply.github.com>
2021-05-03 14:14:00 +0800

    Add files via upload

--- context_id.py
+++ context_id.py
@@ -7,7 +7,7 @@ def remove_articles(t):
     return re.sub("\"","",t)
 
 #open two file
-with open('squad_v3.0.json','r') as file1:
+with open('squad_v3.0_new.json','r') as file1:
     data1 = json.load(file1)
 result1 = data1.copy()
 with open('train-v1.1.json','r') as file2:
@@ -19,21 +19,23 @@ flag = False
 for d in tqdm(result1['data']):
     context = d['paragraphs'][0]['context']
     context_list = context.split(' ',3)[0:2]
-    #print(context_list[0:2])
-    for data in result1['data']:
-        for p in data['paragraphs']:
-            text = p['context']
-            word = text.split(' ',3)[0:2]
-            if word == context_list:
-                p['context_id'] = id_num
-    for data in result2['data']:
-        for p in data['paragraphs']:
-            text = p['context']
-            word = text.split(' ',3)[0:2]
-            if word == context_list:
-                p['context_id'] = id_num
-    id_num += 1
-    '''for dd in result2['data']:
+    '''context = d['paragraphs'][0]['context']
+                context_list = context.split(' ',3)[0:2]
+                #print(context_list[0:2])
+                for data in result1['data']:
+                    for p in data['paragraphs']:
+                        text = p['context']
+                        word = text.split(' ',3)[0:2]
+                        if word == context_list:
+                            p['context_id'] = id_num
+                for data in result2['data']:
+                    for p in data['paragraphs']:
+                        text = p['context']
+                        word = text.split(' ',3)[0:2]
+                        if word == context_list:
+                            p['context_id'] = id_num
+                id_num += 1'''
+    for dd in result2['data']:
         for p in dd['paragraphs']:
             i += 1
             p_list = p['context'].split(' ',3)
@@ -45,10 +47,10 @@ for d in tqdm(result1['data']):
                 d['paragraphs'][0]['context_id'] = id_num
                 id_num += 1
                 #print(p)
-                break'''
+                break
             
 print(i)
-with open('squad_v3.0_pred.json','w') as write_file1:
+with open('squad_v3.0_pred_2.json','w') as write_file1:
     json.dump(result1,write_file1)
-with open('squad_v3.0_data.json','w') as write_file2:
+with open('squad_v3.0_data_2.json','w') as write_file2:
     json.dump(result2,write_file2)
